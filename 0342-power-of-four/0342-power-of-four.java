5260c2a24986e2e1c95ea5ee05b0dd9fdb1c10af
Aswini-t-hub <24pa1a5761@vishnu.edu.in>
2026-08-04 18:13:37 +0530

    Time: 1 ms (89.76%), Space: 42.5 MB (71.09%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+       if(n<1) return false;
+       else if(n==1) return true;
+       while(n%4==0){
+        n=n/4;
+       } 
+       if(n==1) return true;
+       else return false;
+    }
+}
