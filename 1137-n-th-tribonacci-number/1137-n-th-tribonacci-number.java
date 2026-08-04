5cab021aad31fd03242a88a1bf2307cc4709980c
Aswini-t-hub <24pa1a5761@vishnu.edu.in>
2026-08-04 18:11:55 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.03%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+      if(n==0) return 0;
+      else if(n==1) return 1;
+      int first=0;
+      int second=1;
+      int third=1;
+      if(n>1){
+        for(int i=1;i<=n;i++){
+        int forth=first+second+third;
+        first=second;
+        second=third;
+        third=forth;
+      }
+      }
+      return first;  
+    }
+}
